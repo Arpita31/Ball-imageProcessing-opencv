1baffe5eef222d0ef2040beb3fe231e573e8ba36
Arpita Patnaik <arpita1095@gmail.com>
2020-04-25 00:24:55 +0530

    uploading files with images

---
+++ Main.java
@@ -0,0 +1,40 @@
+package application;
+	
+import org.opencv.core.Core;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			
+			System.err.println("FXML resource: " + getClass().getResource("zzdetect.fxml"));
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("zzdetect.fxml"));
+			
+			BorderPane root = (BorderPane)loader.load();
+			Scene scene = new Scene(root,800,700);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setTitle("Object Detection");
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+			zzdetectController controller = loader.getController(); 
+			controller.setRootElement(root);
+			
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME); 
+		launch(args);
+	}
+}
